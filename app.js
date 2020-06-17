c4df8b47bae69b6df47d5d7e2ae80e46bde32890
Abiola <49586787+Abiola-Farounbi@users.noreply.github.com>
2020-06-17 20:47:22 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+document.querySelector('.box').style.display='none';
+
+// preloader
+var loaders;
+function loader(){
+    loaders=setTimeout(showPage,1000)
+}
+function showPage(){
+    document.querySelector('.loading').style.display='none';
+    document.querySelector('.box').style.display='block';
+    document.querySelector('.bgBackground').classList.add('whiteBg')
+   
+}
